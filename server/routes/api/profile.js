b2b1d9798092bb9ba5992809d34f51f57da9d248
GerrardE <ezeugwagerrard@gmail.com>
2019-05-02 20:17:02 +0100

    ft(api): Implement current user route Define api end point and method

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import profile from '../../controllers/profile';
+import authenticate from '../../middlewares/passport';
 
 const router = express.Router();
 
-// @route  GET api/profile/test
-// @desc   Tests profile route
-// @access Public
 router.get('/test', profile.test);
+router.get('/', authenticate, profile.getUser)
 
 export default router;
