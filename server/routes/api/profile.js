fbad033c5514ccf45d7ecb9a98e37c55e0fe5852
GerrardE <ezeugwagerrard@gmail.com>
2019-05-04 21:32:16 +0100

    ft(api): Implement delete route - Define delete education by id method and api - Define delete experience by id method and api - Define delete user/profile by id method and api

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -7,10 +7,13 @@ const router = express.Router();
 router.get('/test', profile.test);
 router.get('/', authenticate, profile.getUser)
 router.post('/', authenticate, profile.createUser)
+router.delete('/', authenticate, profile.deleteUser)
 router.get('/handle/:handle', profile.getProfileByHandle)
 router.get('/user/:user_id', profile.getProfileById)
 router.get('/all', profile.getAllProfiles)
 router.post('/experience', authenticate, profile.addExperience)
+router.delete('/experience/:exp_id', authenticate, profile.deleteExperience)
 router.post('/education', authenticate, profile.addEducation)
+router.delete('/education/:edu_id', authenticate, profile.deleteEducation)
 
 export default router;
