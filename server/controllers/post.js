9fa170353b3d60974d6c8c68be3f3221e2ea0875
GerrardE <ezeugwagerrard@gmail.com>
2019-05-05 15:04:39 +0100

    ft(api): Implement get/delete posts - Define get all posts api endpoint and method - Define get post api endpoint and method - Define delete post api endpoint and method

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,4 +1,4 @@
-import User from '../models/User';
+import Profile from '../models/Profile';
 import Post from '../models/Post';
 import validPost from '../middlewares/post';
 
@@ -12,7 +12,7 @@ class PostController {
     })
   }
 
-  // @route  POST api/post/create
+  // @route  POST api/posts
   // @desc   Creates a post
   // @access Private
   static createPost (req, res) {
@@ -27,7 +27,7 @@ class PostController {
           errors
         })
     }
-    
+
     const newPost = new Post ({
       text: req.body.text,
       name: req.body.name,
@@ -42,6 +42,114 @@ class PostController {
             success: true,
             message: 'Post created successfully.',
             post
+          })
+      })
+  }
+
+  // @route  GET api/posts
+  // @desc   Gets all posts
+  // @access Public
+  static getPosts (req, res) {
+    const errors = {};
+
+    Post.find()
+      .sort({ date: -1 })
+        .then(posts => {
+          return res.status(200)
+            .json({
+              success: true,
+              message: 'Posts retrieved successfully.',
+              posts
+            })
+        })
+        .catch(err => {
+          errors.nopost = 'No posts yet.'
+          return res.status(404)
+            .json({
+              success: false,
+              errors
+            })
+        })
+  }
+
+  // @route  GET api/posts/:id
+  // @desc   Gets a post
+  // @access Public
+  static getPost (req, res) {
+    const errors = {};
+
+    Post.findById(req.params.id)
+        .then(post => {
+          return res.status(200)
+            .json({
+              success: true,
+              message: 'Post retrieved successfully.',
+              post
+            })
+        })
+        .catch(err => {
+          errors.nopost = 'No post found with that ID.'
+          return res.status(404)
+            .json({
+              success: false,
+              errors
+            })
+        })
+  }
+
+  // @route  DELETE api/posts/:id
+  // @desc   Deletes a post
+  // @access Private
+  static deletePost (req, res) {
+    const errors = {};
+
+    Profile.findOne({user: req.user.id})
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+            // Check for post owner
+            if(post.user.toString() !== req.user.id) {
+              errors.notauthorized = 'User not authorized.';
+              return res.status(401)
+                .json({
+                  success: false,
+                  errors
+                })
+            }
+
+            // Delete the post
+            post.remove()
+              .then(() => {
+                return res.status(200)
+                  .json({
+                    success: true,
+                    message: 'Post deleted successfully'
+                  })
+              })
+              .catch(err => {
+                errors.notdeleted = 'Post could not be deleted. Try again.';
+                return res.status(400)
+                  .json({
+                    success: false,
+                    errors
+                  })
+              })
+          })
+          .catch(err => {
+            errors.nopost = 'There is no post with this ID.';
+              return res.status(404)
+                .json({
+                  success: false,
+                  errors
+                })
+          })
+      })
+      .catch(err => {
+        errors.noprofile = 'This profile does not exist.';
+        return res.status(404)
+          .json({
+            success: false,
+            errors
           })
       })
   }
--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -5,5 +5,8 @@ import authenticate from '../../middlewares/passport';
 const router = express.Router();
 
 router.post('/', authenticate, post.createPost);
+router.get('/', post.getPosts);
+router.get('/:id', post.getPost);
+router.delete('/:id', authenticate, post.deletePost);
 
 export default router;
