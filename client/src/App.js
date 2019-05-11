ff45bd611fac6124febad7a761347babedefc55f
GerrardE <ezeugwagerrard@gmail.com>
2019-05-11 12:34:48 +0100

    ft(view): Implement error handling - Handle error messages - Install classnames for bootstrap 'is-invalid'

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from  'react-redux';
+import store from './store';
 import Navbar from './components/layouts/Navbar';
 import Footer from './components/layouts/Footer';
 import Landing from './components/layouts/Landing';
@@ -7,12 +9,12 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import './App.css';
 
-
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
+      <Provider store={ store }>
+        <Router>
+          <div className="App">
           <Navbar />
           {/* remember to include 'exact' */}
             <Route exact path="/" component={ Landing } />
@@ -22,7 +24,8 @@ class App extends Component {
             </div>
           <Footer />
         </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
